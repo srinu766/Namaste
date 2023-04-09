8a4aac54679fbf03562cfa96169c8807526d4c7b
srinu <sopparisrinu637@gmail.com>
2023-04-09 10:27:03 +0530

    first chapter

---
+++ App.js
@@ -0,0 +1,17 @@
+const heading1 = React.createElement("h1", {
+    id:"title",
+}, "heading 1");
+
+const heading2 = React.createElement("h2",{
+    id:"title",
+},"heading 2");
+
+const container = React.createElement("div", {
+    id:"container",
+},[heading1, heading2]);
+
+console.log(heading1);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
